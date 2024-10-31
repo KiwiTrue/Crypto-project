5982af642a62f96fe4c87d4df9a04a133936e519
Faisal <93672092+KiwiTrue@users.noreply.github.com>
2024-10-31 13:13:09 +0300

    Update Codemaster.py

--- Codemaster.py
+++ Codemaster.py
@@ -1,8 +1,10 @@
-
 # Codemaster.py
 
 import socket
 import threading
+import atexit
+import struct
+import json
 from CryptoFunctions import encrypt, decrypt
 from GameLogic import generate_random_sequence, evaluate_guess
 from CertificationAuthority import CertificationAuthority
@@ -14,34 +16,91 @@ class Codemaster:
         self.clients = []
         self.secret_sequence = self.generate_sequence()
         self.ca = CertificationAuthority()
-        self.private_key, self.public_key = self.ca.generate_keys()
-        self.certificate = self.ca.certify_user(self.public_key)
+        self.private_key, self.public_key = CertificationAuthority.generate_keys()
+        self.certificate = CertificationAuthority.certify_user(self.public_key)
+        self.running = True
+        atexit.register(self.stop_server)
 
     def start_server(self):
         server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         server.bind((self.host, self.port))
         server.listen()
         print('Codemaster server started.')
-        while True:
-            client_socket, addr = server.accept()
-            print(f'Player connected from {addr}')
-            self.clients.append(client_socket)
-            threading.Thread(target=self.handle_client, args=(client_socket,)).start()
+        while self.running:
+            try:
+                client_socket, addr = server.accept()
+                print(f'Player connected from {addr}')
+                self.clients.append(client_socket)
+                threading.Thread(target=self.handle_client, args=(client_socket,)).start()
+            except OSError:
+                break
+
+    def stop_server(self):
+        self.running = False
+        for client in self.clients:
+            client.close()
+        print('Codemaster server stopped.')
 
     def generate_sequence(self):
         colors = ['red', 'green', 'blue', 'yellow', 'orange', 'purple']
         return generate_random_sequence(colors, 4)
 
     def handle_client(self, client_socket):
-        while True:
-            encrypted_guess = client_socket.recv(1024)
-            guess = self.receive_encrypted_message(encrypted_guess)
-            feedback = self.process_guess(guess)
-            encrypted_feedback = self.send_encrypted_message(feedback)
-            client_socket.sendall(encrypted_feedback)
-            if feedback['win']:
-                self.declare_winner(client_socket)
+        # Receive client's certificate length
+        data = self.receive_all(client_socket, 4)
+        if not data:
+            client_socket.close()
+            return
+        client_cert_len = struct.unpack('>I', data)[0]
+        # Receive client's certificate
+        client_cert = self.receive_all(client_socket, client_cert_len)
+        if not client_cert:
+            client_socket.close()
+            return
+        # Send own certificate with length
+        cert_len = len(self.certificate)
+        client_socket.sendall(struct.pack('>I', cert_len))
+        client_socket.sendall(self.certificate)
+        # Validate client's certificate
+        client_public_key = CertificationAuthority.validate_certificate(client_cert)
+        if not client_public_key:
+            print("Failed to validate client's certificate.")
+            client_socket.close()
+            return
+        while self.running:
+            try:
+                # Receive message length
+                data = self.receive_all(client_socket, 4)
+                if not data:
+                    break
+                msg_len = struct.unpack('>I', data)[0]
+                # Receive encrypted guess
+                encrypted_guess = self.receive_all(client_socket, msg_len)
+                guess_json = decrypt(encrypted_guess, self.private_key, cipher='rsa')
+                guess = json.loads(guess_json)
+                feedback = self.process_guess(guess)
+                feedback_json = json.dumps(feedback)
+                encrypted_feedback = encrypt(feedback_json, client_public_key, cipher='rsa')
+                # Send feedback with length
+                feedback_len = len(encrypted_feedback)
+                client_socket.sendall(struct.pack('>I', feedback_len))
+                client_socket.sendall(encrypted_feedback)
+                if feedback['win']:
+                    self.declare_winner(client_socket)
+                    break
+            except Exception as e:
+                print(f"Error handling client: {e}")
                 break
+        client_socket.close()
+
+    def receive_all(self, sock, n):
+        data = b''
+        while len(data) < n:
+            packet = sock.recv(n - len(data))
+            if not packet:
+                return None
+            data += packet
+        return data
 
     def process_guess(self, guess):
         exact, color = evaluate_guess(self.secret_sequence, guess)
