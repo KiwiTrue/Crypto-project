493bdb186cf351962beefa1c776b7ea7bef25fcc
Faisal <93672092+KiwiTrue@users.noreply.github.com>
2024-10-31 09:44:46 +0000

    fixed main.py

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 # Main.py
 
 import threading
+import os
 from Codemaster import Codemaster
 from GUI import GameGUI
 
@@ -14,4 +15,7 @@ def start_game_gui():
 
 if __name__ == '__main__':
     threading.Thread(target=start_codemasters).start()
-    threading.Thread(target=start_game_gui).start()
+    if os.environ.get('DISPLAY'):
+        threading.Thread(target=start_game_gui).start()
+    else:
+        print("No display found. Skipping GUI.")
